e18952385a6867ed1d0b608e7553dcf53e6fbc33
iz <lysk9884@gmail.com>
2026-09-22 21:38:51 +0900

    chore(wiki): WEB을 공개 서브모듈 seoul-dengoku-wiki로 분리한다

--- WEB/wiki/src/components/StateFlag.tsx
+++
@@ -1,4 +0,0 @@
-export function StateFlag({ stateId, title }: { stateId: string; title?: string }) {
-  const normalizedId = /^S(?:0[1-9]|1[0-6])$/u.test(stateId) ? stateId : 'S01'
-  return <img className="state-flag" src={`${import.meta.env.BASE_URL}state-flags/${normalizedId}.webp?v=20260920-1`} alt={title ?? ''} loading="lazy" decoding="async" />
-}
